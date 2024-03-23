0f2162e834bb51e12989899ec42192796d321106
sandy <sandeshjadhav2002@gmail.com>
2024-03-23 23:19:13 +0530

    [Feat]: Dashboard Setup & Context

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useContext, useEffect } from 'react'
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
+import Login from "./components/Login"
+import Dashboard from "./components/Dashboard"
+import Messages from "./components/Messages"
+import Doctors from "./components/Doctors"
+import AddNewDoctors from "./components/AddNewDoctor"
+import AddNewAdmin from "./components/AddNewAdmin"
+import Sidebar from "./components/Sidebar"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {Context} from "./main"
+import axios from"axios"
+
+const App = () => {
+
+  const {isAuthenticated,setIsAuthenticated,setUser} = useContext(Context)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/admin/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+  return (
+    <>
+    <Router>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/doctor/addnew' element={<AddNewDoctors/>}/>
+        <Route path='/admin/addnew' element={<AddNewAdmin/>}/>
+        <Route path='/messages' element={<Messages/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+      </Routes>
+      <ToastContainer position="top-center" />
+    </Router>
+    </>
+  )
+}
+
+export default App
