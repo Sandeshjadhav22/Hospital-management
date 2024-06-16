a8cd069f2ff80266101f914d771bc73e3627c910
sandy <sandeshjadhav2002@gmail.com>
2024-06-17 00:06:15 +0530

    dashboard api update

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://hospital-management-backend-seven.vercel.app/api/v1/user/admin/me",
           { withCredentials: true }
         );
         setIsAuthenticated(true);
--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
     const fetchAppointments = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/appoinment/getall",
+          "https://hospital-management-backend-seven.vercel.app/api/v1/appoinment/getall",
           { withCredentials: true }
         );
         setAppointments(data.appointments);
@@ -27,7 +27,7 @@ const Dashboard = () => {
   const handleUpdateStatus = async (appointmentId, status) => {
     try {
       const { data } = await axios.put(
-        `http://localhost:4000/api/v1/appoinment/update/${appointmentId}`,
+        `https://hospital-management-backend-seven.vercel.app/api/v1/appoinment/update/${appointmentId}`,
         { status },
         { withCredentials: true }
       );
